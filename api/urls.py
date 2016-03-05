fc91218159b60129fae6716818dbc5a1d5d74ded
Tobias Preuss <tobias.preuss@googlemail.com>
2016-03-05 18:49:56 +0100

    Enable searching by GET parameters.
    + The API endpoint /search allows to append GET parameters to the url.
      Parameters can be taken from the "properties" object of each tree.
      Multiple parameters are concatenated by logical AND.

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url, include
 
 from rest_framework import routers
 
-from .views import TreeViewSet
+from .views import TreeViewSet, SearchViewSet
 
 router = routers.DefaultRouter()
 router.register(r'trees', TreeViewSet)
+router.register(r'search', SearchViewSet, base_name='search')
+
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
+from django.db.models import Q
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework_gis.pagination import GeoJsonPagination
 
-from .models import Tree
+from .models import Tree, Attribute
 from .serializers import TreeSerializer
 
 
@@ -15,3 +18,30 @@ class TreeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tree.objects.all()
     serializer_class = TreeSerializer
     pagination_class = TreePagination
+
+
+class SearchViewSet(viewsets.ReadOnlyModelViewSet):
+
+    pagination_class = TreePagination
+
+    def list(self, request, format=None):
+        tree_ids = self.__tree_ids_from_query_params(request.query_params)
+        queryset = Tree.objects.filter(id__in=tree_ids)
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer_class = TreeSerializer(paginated_queryset, many=True)
+        return Response(serializer_class.data, status=status.HTTP_200_OK)
+
+    def __tree_ids_from_query_params(self, query_params):
+        united_tree_ids = {}
+        for i, query_param_key in enumerate(query_params.keys()):
+            query_param_value = query_params.get(query_param_key, None)
+            attribute_queryset = Attribute.objects.filter(key=query_param_key) \
+                .filter(Q(string__string_value=query_param_value))
+            filtered_tree_ids = {a.tree_id for a in attribute_queryset}
+            if i == 0:
+                # Init the set
+                united_tree_ids = filtered_tree_ids
+            else:
+                # Intersect both sets (logical AND)
+                united_tree_ids = united_tree_ids & filtered_tree_ids
+        return united_tree_ids
