44e322634576e266928595488766a639b8f1ac0f
Jochen Klar <jochenklar@gmail.com>
2016-03-07 20:52:30 +0100

    small fix

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from .views import TreeViewSet, LocationViewSet, SearchViewSet
 
 router = routers.DefaultRouter()
-router.register(r'trees', ListViewSet, base_name='tree')
+router.register(r'trees', TreeViewSet, base_name='tree')
 router.register(r'location', LocationViewSet, base_name='location')
 router.register(r'search', SearchViewSet, base_name='search')
 
